931ce0d7928d49d74d632127e03a0c27d7b3ec2a
唐子君 <223663737@qq.com>
2020-05-13 21:43:31 +0800

    asda

--- my-cli.js
+++ my-cli.js
@@ -4,6 +4,11 @@
 const path = require("path");
 const bootstrap = require("commitizen/dist/cli/git-cz").bootstrap;
 
+console.info(
+  "~~~~~~~~~",
+  path.join(__dirname, "../../node_modules/commitizen")
+);
+
 bootstrap({
   cliPath: path.join(__dirname, "../../node_modules/commitizen"),
   // this is new
