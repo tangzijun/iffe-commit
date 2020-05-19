435ec18577a86b8f6984aa52f843449e3c91a598
唐子君 <223663737@qq.com>
2020-05-19 15:11:27 +0800

    fix: 修改

--- index.js
+++ index.js
@@ -12,14 +12,19 @@ const pkgUrl = `${process.cwd()}/package.json`;
 let file = editJsonFile(pkgUrl);
 const pakName = "iffe-commit";
 
+// 添加scripts脚本
 file.set("scripts.commit", "git add . && git-cz");
+file.set("scripts.push", "git push origin dev");
+file.set("scripts.pull", "git pull origin dev --rebase");
 
+// 添加commitizen配置
 file.set("config.commitizen.path", "node_modules/cz-customizable");
 file.set(
   "config.cz-customizable.config",
   `node_modules/${pakName}/cz-config.js`
 );
 
+// 添加husky配置
 file.set(
   "husky.hooks.commit-msg",
   `commitlint --config node_modules/${pakName}/commitlint.config.js -E HUSKY_GIT_PARAMS`
